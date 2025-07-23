54fa2590b164e194f43c8a00d1d5661e5caf7ef0
Frost Hu <894785732@qq.com>
2025-07-23 16:46:43 +0800

    优化弹幕开关快捷键逻辑，修复进度条预览图跳动bug

--- app/src/main/kotlin/dev/aaa1115910/bv/component/controllers2/VideoShot.kt
+++ app/src/main/kotlin/dev/aaa1115910/bv/component/controllers2/VideoShot.kt
@@ -1,11 +1,13 @@
 package dev.aaa1115910.bv.component.controllers2
 
+import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -13,6 +15,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.draw.shadow
@@ -58,10 +61,12 @@ fun VideoShot(
         if (!view.isInEditMode) {
             bitmap = videoShot.getImage(position.toInt() / 1000).asImageBitmap()
         }
-        val baseOffset = -imageWidth / 2
-        val imageOffset = baseOffset + screenWidth * (position.toFloat() / duration.toFloat())
-        coercedImageOffset =
-            imageOffset.coerceIn(0.dp + coercedOffset, screenWidth - imageWidth - coercedOffset)
+        if(imageWidth != 0.dp){
+            val baseOffset = -imageWidth / 2
+            val imageOffset = baseOffset + screenWidth * (position.toFloat() / duration.toFloat())
+            coercedImageOffset =
+                imageOffset.coerceIn(0.dp + coercedOffset, screenWidth - imageWidth - coercedOffset)
+        }
     }
 
 //    if (view.isInEditMode) {
@@ -71,11 +76,13 @@ fun VideoShot(
     BoxWithConstraints(
         modifier = modifier.fillMaxWidth()
     ) {
-        screenWidth = maxWidth
         VideoShotImage(
             modifier = Modifier
                 .offset(x = coercedImageOffset)
+                .alpha(if (coercedImageOffset != 0.dp) 1f else 0f)
                 .onSizeChanged {
+                    Log.d("VideoShot", "image width: ${it.width}")
+                    screenWidth = maxWidth
                     imageWidth = with(density) { it.width.toDp() }
                 },
             bitmap = bitmap
@@ -92,6 +99,7 @@ fun VideoShotImage(
 
     Image(
         modifier = modifier
+            .width(178.dp)
             .height(100.dp)
             .shadow(4.dp, MaterialTheme.shapes.large)
             .clip(MaterialTheme.shapes.large)
