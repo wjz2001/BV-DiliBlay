50c937262386a0eab88654e1595e36c321341bfc
Frost Hu <894785732@qq.com>
2025-11-27 16:09:15 +0800

    update 版本号 & README.md

--- buildSrc/src/main/kotlin/AppConfiguration.kt
+++ buildSrc/src/main/kotlin/AppConfiguration.kt
@@ -8,7 +8,7 @@ object AppConfiguration {
     const val targetSdk = 36
     private const val major = 0
     private const val minor = 3
-    private const val patch = 8
+    private const val patch = 9
     private const val hotFix = 0
 
     @Suppress("KotlinConstantConditions")
