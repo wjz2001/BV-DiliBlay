c2d1e8c4c47c6c19a11b98ad73e499da8f80aaca
wjz2001 <wjz2001@users.noreply.github.com>
2026-01-31 15:03:48 +0800

    视频详情页面收藏、点赞、投币会显示相关数据

--- app/src/main/kotlin/dev/aaa1115910/bv/component/buttons/CoinButton.kt
+++ app/src/main/kotlin/dev/aaa1115910/bv/component/buttons/CoinButton.kt
@@ -1,5 +1,6 @@
 package dev.aaa1115910.bv.component.buttons
 
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Paid
 import androidx.compose.material.icons.rounded.Paid
@@ -13,15 +14,30 @@ import androidx.tv.material3.Icon
 fun CoinButton(
     modifier: Modifier = Modifier,
     isCoined: Boolean,
+    countText: String = "",
     onClick: () -> Unit
 ) {
+
+    val pillShape = RoundedCornerShape(percent = 50)
+
     Button(
         modifier = modifier,
         onClick = onClick,
     ) {
+        /*
         Icon(
             imageVector = if (isCoined) Icons.Rounded.Paid else Icons.Outlined.Paid,
             contentDescription = null
+        )
+         */
+        CapsuleStatButtonContent(
+            icon = {
+                Icon(
+                    imageVector = if (isCoined) Icons.Rounded.Paid else Icons.Outlined.Paid,
+                    contentDescription = null
+                )
+            },
+            text = countText
         )
     }
 }
